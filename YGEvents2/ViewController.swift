d62e08ef8081fe0e9337c8783e3a217e3dcdd94d
Jonathan Brook <JonathanBrook@jonathans-air.fritz.box>
2018-06-10 13:45:52 +1200

    Initial Commit

---
+++ YGEvents2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YGEvents2
+//
+//  Created by Jonathan Brook on 10/06/18.
+//  Copyright © 2018 Jonathan Brook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
